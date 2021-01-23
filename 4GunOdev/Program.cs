3c4cd477d2aefdf0d64cb29ecc9f46590e8a5fc0
Enes <73299223+enescelep@users.noreply.github.com>
2021-01-23 17:37:50 +0300

    Masaüstündeki isimler.txt dosyasından il ve plakaları çekiyoruz.

--- 4GunOdev/Program.cs
+++ 4GunOdev/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 
 namespace _4GunOdev
 {
@@ -7,32 +8,61 @@ namespace _4GunOdev
     {
         static void Main(string[] args)
         {
+            
             MyDictionary<int, string> myDictionary = new MyDictionary<int, string>();
-            List<int> plakalar = new List<int> {};
-            List<string> iller = new List<string> {"Adana","Adıyaman","Afyon","Ağrı","Amasya","Ankara","Antalya","Artvin","Aydın","Balıkesir"};
-            /*myDictionary.Add(1, "adana");
-            myDictionary.Add(5, "amasya");
-            myDictionary.Add(34, "istanbul");
-            myDictionary.Add(6, "antalya");
-            Console.WriteLine(myDictionary.Length);
-            Console.WriteLine(myDictionary.GetKey("istanbul"));
-            Console.WriteLine(myDictionary.GetValue(34));*/
-
-            for (int i = 1; i <= plakalar.Count; i++)
+            /*List<int> plakalar = new List<int>();
+            List<string> iller = new List<string>() {"adana","istanbul","ankara" };
+            for (int i = 1; i <= iller.Count; i++)
             {
                 plakalar.Add(i);
             }
-            for (int i = 0; i < plakalar.Count; i++)
+            for (int i = 0; i < iller.Count; i++)
             {
                 myDictionary.Add(plakalar[i], iller[i]);
             }
-            Console.WriteLine(myDictionary.Length);
-            Console.WriteLine("-------------------");
+            Console.WriteLine(myDictionary.GetValue(2));*/
 
-            for (int i = 0; i < myDictionary.Length; i++)
+            string[] arguments;
+            try
             {
-                int plakaSayisi = plakalar[i];
-                Console.WriteLine(plakaSayisi + " | " + myDictionary.GetValue(plakaSayisi));
+                FileStream liste = new FileStream(@"C:\Users\user\Desktop\iller.txt", FileMode.Open, FileAccess.Read);
+                StreamReader reader = new StreamReader(liste);
+                string line;
+                List<int> plakalar = new List<int>();
+                List<string> iller = new List<string>();
+                while ((line = reader.ReadLine())!=null)
+                {
+                    arguments = line.Split(':');
+                    plakalar.Add(Convert.ToInt32(arguments[0]));
+                    iller.Add(arguments[1]);
+                }
+                for (int i = 0; i < plakalar.Count; i++)
+                {
+                    myDictionary.Add(plakalar[i], iller[i]);
+                }
+            }
+
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            int hak = 5;
+            while (hak!=0)
+            {
+                try
+                {
+                    Console.WriteLine("Plaka giriniz: ");
+                    int plaka = Convert.ToInt32(Console.ReadLine());
+                    Console.WriteLine(myDictionary.GetValue(plaka));
+
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+
+                    // break;
+                }
+                finally { hak--; }
             }
         }
     }
